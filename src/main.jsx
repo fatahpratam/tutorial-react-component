29de9e32b41c695cfe603b7566a31ef9acaa14a1
fatahpratam <fatahpratamasolutions@gmail.com>
2024-11-23 20:17:45 +0700

    Move an import from main to Header

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './component/App.jsx'
-import './header.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
